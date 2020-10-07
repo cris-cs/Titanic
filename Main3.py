8432ac0a56773571f5a83de2f7d54ec62fdb53b8
XabMS <xabier94@gmail.com>
2020-10-07 22:20:25 +0200

    mejorando

--- Main3.py
+++ Main3.py
@@ -3,14 +3,22 @@ from Titanic01 import sexe, survided, name
 nbPassagers = len(sexe)
 
 
-def analyseTitanic(totalPassagers):
+def analyseTitanic(totalPassagers, sexePassager):
     nbSurvivants = 0
+    nbPassagersCritere = 0
     for passager in range(nbPassagers):
         if survided[passager] == 1:
             nbSurvivants += 1
-    return nbSurvivants
+            if sexe[passager] == sexePassager:
+                nbPassagersCritere += 1
+
+    taux = nbPassagersCritere / nbSurvivants * 100
+    return taux
 
 
-print(analyseTitanic(nbPassagers))
-tauxSurvie = analyseTitanic(nbPassagers) / nbPassagers * 100
-print(f'{tauxSurvie:.2f}%')
+#print(analyseTitanic(nbPassagers))
+#tauxSurvie = analyseTitanic(nbPassagers) / nbPassagers * 100
+#print(f'{tauxSurvie:.1f}%')
+
+print(analyseTitanic(nbPassagers, 'male'))
+print(analyseTitanic(nbPassagers, 'female'))
