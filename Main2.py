3592f017afeac16787c5d36d7decbda8c844e405
XabMS <xabier94@gmail.com>
2020-10-07 21:22:40 +0200

    ejercicio 2.3 fini

--- Main2.py
+++ Main2.py
@@ -5,7 +5,15 @@ nom = 'Flynn, Mr. James'
 print(dictTitanic[nom]['Pclass'])
 
 nom2 = 'Todoroff, Mr. Lalio'
+
 if dictTitanic[nom2]['SibSp'] > 0:
     print('he was not alone')
 else:
     print('he was alone')
+
+nom3 = 'Nicola-Yarred, Miss. Jamila'
+
+if nom3 in dictTitanic:
+    print('She was here')
+else:
+    print('she was not here')
